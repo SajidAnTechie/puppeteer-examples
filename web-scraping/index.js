c58bcafbaa7502ddc4316c6135bc07dfceb0b4c7
Sajid Ansari <sajidansari33272@gmail.com>
2021-03-09 20:55:43 +0545

    web-scraping for books details modified

--- web-scraping/index.js
+++ web-scraping/index.js
@@ -5,20 +5,22 @@ const fs = require("fs");
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-
+  const url = "https://www.okdam.com/category/books";
   try {
-    await page.goto("https://www.okdam.com/category/books");
+    await page.goto(url);
 
-    const result = await page.$$eval(".product_name", (data) => {
+    const results = await page.$$eval(".product-inner", (data) => {
       return [...data].map((d) => {
         return {
-          bookName: d.innerText,
+          title: d.querySelector(".product_name").innerText.trim(),
+          rating: d.querySelector("span.rating-count").innerText.trim(),
+          price: d.querySelector("span.og-price").innerText.trim(),
         };
       });
     });
 
     const parser = new Parser();
-    const csv = parser.parse(result);
+    const csv = parser.parse(results);
 
     fs.writeFileSync("./data.csv", csv);
   } catch (error) {
